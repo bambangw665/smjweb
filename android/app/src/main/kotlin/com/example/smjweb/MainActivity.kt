e896af9bdfd4d65f656afaf7e2103a7694535522
bambangw665 <bambangw665@gmail.com>
2022-08-31 15:02:35 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/smjweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smjweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/HomePage.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/smj_jumbotronIMG.jpg"))),
+      ),
+    );
+  }
+}
